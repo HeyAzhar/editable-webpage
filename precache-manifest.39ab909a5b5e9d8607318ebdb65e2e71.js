bd526f953a7a5d22b071aaf30e4bcbc20bdb467f
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-08-17 04:17:01 +0530

    Updates

---
+++ precache-manifest.39ab909a5b5e9d8607318ebdb65e2e71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17999e12a95ad1a897e3534f27fe4bc4",
+    "url": "/editable-webpage/index.html"
+  },
+  {
+    "revision": "5810816921c73664860d",
+    "url": "/editable-webpage/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9c3d9018d659d585a4c4",
+    "url": "/editable-webpage/static/css/main.9fec9b49.chunk.css"
+  },
+  {
+    "revision": "5810816921c73664860d",
+    "url": "/editable-webpage/static/js/2.9052419d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/editable-webpage/static/js/2.9052419d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c3d9018d659d585a4c4",
+    "url": "/editable-webpage/static/js/main.8a5162b2.chunk.js"
+  },
+  {
+    "revision": "594ce218e21153c04f62",
+    "url": "/editable-webpage/static/js/runtime-main.cde49db3.js"
+  },
+  {
+    "revision": "2cb28e1b81c2d8d5d1c0e604bd157a51",
+    "url": "/editable-webpage/static/media/heroSec.2cb28e1b.jpg"
+  }
+]);
